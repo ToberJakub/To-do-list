cc78d064e7647bb3826c66063be8d62472ed07c7
Jakub Tober <jakubtober276@gmail.com>
2022-06-06 21:15:47 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+{
+    const tasks = [
+        {
+            content: "Nauczyć się programowania",
+            done: false,
+        },
+        {
+            content: "Zjeść Kolację",
+            done: true,
+        },
+    ];
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    };
+   const init = () => {
+       render();
+   };
+   init();
+
+    
+}
